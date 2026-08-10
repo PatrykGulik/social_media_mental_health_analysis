528b4649eac35704499c552e3f150dd91e892aae
gulik93 <patrykgulik@hotmail.co.uk>
2026-08-10 16:50:56 +0100

    Calculated the effect size using Cohen's d to explore practical significance of results

--- aism_ds_pg.py
+++ aism_ds_pg.py
@@ -49,20 +49,73 @@ def scatter_plot(df, x_col, y_col, title, xlabel, ylabel):
 #scatter_plot(df, 'Social_Isolation_Score', 'Mental_Health_Score', 'Relationship between Social Isolation and Mental Health', 'Social Isolation Score', 'Mental Health Score')
 #scatter_plot(df, 'Physical_Activity_Hours', 'Mental_Health_Score', 'Relationship between Physical Activity and Mental Health', 'Physical Activity Hours', 'Mental Health Score')
 
-#### Correlation Analysis
+# Functions for Analysis
 
 def correlation_analysis(df):
     social_media_corr = df['Daily_Social_Media_Hours'].corr(df['Mental_Health_Score'])
     sleep_corr = df['Sleep_Hours'].corr(df['Mental_Health_Score'])
     social_isolation_corr = df['Social_Isolation_Score'].corr(df['Mental_Health_Score'])
     physical_activity_corr = df['Physical_Activity_Hours'].corr(df['Mental_Health_Score'])
-    print(f"Correlation between Daily Social Media Hours and Mental Health Score: {social_media_corr}")
-    print(f"Correlation between Sleep Hours and Mental Health Score: {sleep_corr}")
-    print(f"Correlation between Social Isolation Score and Mental Health Score: {social_isolation_corr}")
-    print(f"Correlation between Physical Activity Hours and Mental Health Score: {physical_activity_corr}")
+    return social_media_corr, sleep_corr, social_isolation_corr, physical_activity_corr
 
-correlation_analysis(df)
 
-correlation, p_value = pearsonr(df['Daily_Social_Media_Hours'], df['Mental_Health_Score'])
+def pearsons_correlation(df, col1, col2):
+    correlation, p_value = pearsonr(df[col1], df[col2])
+    return correlation, p_value
+
+def split_social_media_groups(df):
+    mean_social_media  = df['Daily_Social_Media_Hours'].mean()
+
+    high_users  = df[df['Daily_Social_Media_Hours'] > mean_social_media]
+    low_users  = df[df['Daily_Social_Media_Hours'] <= mean_social_media]
+    
+    return high_users, low_users 
+
+def mean_social_media_hours(df):
+    mean_high = high_users['Mental_Health_Score'].mean()
+    mean_low = low_users['Mental_Health_Score'].mean()
+
+    return mean_high, mean_low
+
+def calculate_mental_health_std(high_group, low_group):
+    std_high = high_group['Mental_Health_Score'].std()
+    std_low = low_group['Mental_Health_Score'].std()
+
+    return std_high, std_low
+
+def cohens_d(high_group, low_group):
+    mean_high = high_group['Mental_Health_Score'].mean()
+    mean_low = low_group['Mental_Health_Score'].mean()
+    pooled_std = np.sqrt((high_group['Mental_Health_Score'].var() + low_group['Mental_Health_Score'].var()) / 2)
+    d = (mean_high - mean_low) / pooled_std
+    return d
+
+# Analysis 
+
+social_media_corr, sleep_corr, social_isolation_corr, physical_activity_corr = correlation_analysis(df)
+correlation, p_value = pearsons_correlation(df, 'Daily_Social_Media_Hours', 'Mental_Health_Score')
+high_users, low_users = split_social_media_groups(df)
+mean_high, mean_low = mean_social_media_hours(df)
+std_high, std_low = calculate_mental_health_std(high_users, low_users)
+cohens_d(high_users, low_users)
+
+
+#Results
+
+# Correlation Analysis
+print(f"Correlation between Daily Social Media Hours and Mental Health Score: {social_media_corr}")
+print(f"Correlation between Sleep Hours and Mental Health Score: {sleep_corr}")
+print(f"Correlation between Social Isolation Score and Mental Health Score: {social_isolation_corr}")
+print(f"Correlation between Physical Activity Hours and Mental Health Score: {physical_activity_corr}")
+
+# Pearson's Correlation
 print(f"Correlation: {correlation}")
 print(f"P-value: {p_value:.20e}")
+
+# Mean Mental Health Scores
+print(f"Mean mental health score for high social media users: {mean_high}")
+print(f"Mean mental health score for low social media users: {mean_low}")
+
+# Standard Deviation of Mental Health Scores
+print(f"High social media SD: {std_high}")
+print(f"Low social media SD: {std_low}")
